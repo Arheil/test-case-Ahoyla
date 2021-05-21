9cccea85329e57fd1c880917616d7c7532849715
Arheil <50764766+Arheil@users.noreply.github.com>
2021-05-21 17:17:49 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const input = document.querySelectorAll('.w-input');
 const check = document.querySelector('.check');
 const clear = document.querySelector('.clear');
 const show = document.querySelector('.show');
+const playButton = document.querySelector('.PlayPause3054584993__button');
+const range = document.querySelector('.ProgressBar1832067963__range');
+
+let audio = new Audio('https://music.wixstatic.com/mp3/20b4ac_4de89c3d61f1449f865ba3eb8e14c8b9.mp3');
 
 const showData = () => {
  	input.forEach( i => {
@@ -25,3 +29,12 @@ const checkData = () => {
 check.addEventListener('click', checkData);
 show.addEventListener('click', showData);
 clear.addEventListener('click', clearData);
+
+playButton.addEventListener('click', () => {
+	audio.paused ? audio.play() : audio.pause();
+});
+audio.addEventListener('timeupdate', () => {
+	let p = audio.currentTime / audio.duration;
+
+	range.value = p * 100;
+})
